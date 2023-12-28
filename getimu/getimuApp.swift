31ce31e05130b9fb8633cd5ca1b014fd6545e6cc
cuixh0 <cuixh0@163.com>
2023-12-28 19:55:58 +0800

    Initial Commit

---
+++ getimu/getimuApp.swift
@@ -0,0 +1,17 @@
+//
+//  getimuApp.swift
+//  getimu
+//
+//  Created by cui on 2023/12/28.
+//
+
+import SwiftUI
+
+@main
+struct getimuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
